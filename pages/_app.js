22be6594221838cecc297ac13a90ef8898e747bc
javier <javier.alberto.lugo@gmail.com>
2021-05-12 16:49:11 -0400

    limpiando configuraciones por defecto de nextJs

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ export default function MyApp(props) {
     <ApolloProvider client={client}>
       <CacheProvider value={cache}>
         <Head>
-          <title>My page</title>
+          <title>ePayco</title>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
         <ThemeProvider theme={theme}>
